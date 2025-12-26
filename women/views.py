f4988d3cc70b0a7267b7510b6e360d015e707c8a
Nurlanbekovaaidana <Nurlanbekovaaidana461@gmail.com>
2025-12-26 14:08:47 +0600

    Add Django REST Framework

--- women/views.py
+++ women/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
-from django.shortcuts import render
-from.models import Women
+from .serializers import WomenSerializer
+from .models import Women
 
 class WomenAPIView(generics.ListAPIView):
     queryset = Women.objects.all()
